63edcb030c13d806d192e8d1e97ad9c1266a7f3c
lanYangweb <silvanaxiong@qq.com>
2025-10-25 08:19:31 +0200

    add span.hpp

--- CPP_08/ex01/includes/Span.hpp
+++ CPP_08/ex01/includes/Span.hpp
@@ -1,11 +1,13 @@
 #pragma once
+# include <vector>
 
 class Span {
     private:
         unsigned int _N;
+        std::vector<int> v;
     public:
         void addNumber();
         unsigned int shortestSpan();
         unsigned int longestSpan();
 
-}
+};
